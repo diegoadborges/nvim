bd74871bbf344018722142713d098e48f19f6273
Diego Borges <diegoaragao465@gmail.com>
2024-04-19 10:07:40 -0300

    refactor: new keybinds to move buffer

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,6 +7,13 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map({ "n", "i", "v" }, "[b", "<cmd> bprev <cr>")
-map({ "n", "i", "v" }, "]b", "<cmd> bnext <cr>")
+
+map("n", "]b", function()
+  require("nvchad.tabufline").next()
+end, { desc = "Buffer Goto next" })
+
+map("n", "[b", function()
+  require("nvchad.tabufline").prev()
+end, { desc = "Buffer Goto prev" })
+
 map({ "n", "i", "v" }, "<C-s>", "<cmd> wa <cr>")
