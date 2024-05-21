175ef73567a9e4eabf32612314a05d689266ceb1
Diego Borges <diegoaragao465@gmail.com>
2024-05-21 13:43:35 -0300

    fix: configuration venv selector

--- lua/plugins/venv_selector.lua
+++ lua/plugins/venv_selector.lua
@@ -2,9 +2,9 @@ return {
   "linux-cultist/venv-selector.nvim",
   dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
   opts = {
-    name = {},
+    name = { ".venv" },
   },
-  event = "VimEnter", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
   keys = {
     -- Keymap to open VenvSelector to pick a venv.
     { "<leader>vs", "<cmd>VenvSelect<cr>" },
